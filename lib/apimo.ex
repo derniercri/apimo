d42a019c27ff95eb3054c065969ea77ab6221223
laibulle <laibulle@gmail.com>
2018-07-17 12:21:25 +0200

    create data

--- lib/apimo.ex
+++ lib/apimo.ex
@@ -14,9 +14,48 @@ defmodule Apimo do
     ]
   end
 
-  def fetch_agencies() do
-    case HTTPoison.get("#{@endpoint}/agencies", [], options()) do
+  defp handle_response(res) do
+    case res do
       {:ok, res} -> res.body |> Poison.decode(keys: :atoms)
     end
+  end
+
+  def fetch_agencies() do
+    HTTPoison.get("#{@endpoint}/agencies", [], options()) |> handle_response()
+  end
+
+  def fetch_agency_properties(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/properties", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_contacts(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/contacts", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_leads(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/leads", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_residences(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/residences", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_requests(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/requests", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_contracts(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/contracts", [], options())
+    |> handle_response()
+  end
+
+  def fetch_agency_actions(agency_id) do
+    HTTPoison.get("#{@endpoint}/agencies/#{agency_id}/actions", [], options())
+    |> handle_response()
   end
 end
